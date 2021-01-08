618fe33a58d6398d3265d1e17c0271bfab039f18
wangfeng <wffgy@sohu.com>
2021-01-08 15:49:52 +0800

    外链接视频表格查询

--- video/app/admin.py
+++ video/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.model.video import Video
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['id','name','video_type','from_to','nationality','info','createdtm','updatedtm']
+    ordering = ['id']
+    list_per_page = 10
+    search_fields = ['name']
+    readonly_fields = ['created_time','updated_time']
